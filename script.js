10220c3894d2d020042be94172116093e5901b17
shmagioto66-crypto <shmagioto66@gmail.com>
2026-08-08 00:04:46 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('bookingForm').addEventListener('submit', function(e) {
+    // ვაჩვენებთ შეტყობინებას მომხმარებელს
+    alert('მადლობა! თქვენი ვიზიტი წარმატებით დაიჯავშნა. მენეჯერი მალე დაგიკავშირდებათ.');
+});
